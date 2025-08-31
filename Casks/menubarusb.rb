d76184ae632db8f0d8d44d41419fb3ce1c921e52
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2025-08-31 15:35:19 -0300

    Update menubarusb.rb

--- Casks/menubarusb.rb
+++ Casks/menubarusb.rb
@@ -2,7 +2,7 @@ cask "menubarusb" do
   version "1.5"
   sha256 "15859f0a76151b05c8f5f55d6d7567c61443233c17a4f3ae188a8e3468ce1f93"
 
-  url "https://github.com/rafaelSwi/MenuBarUSB/releases/download/v#{version}/MenuBarUSB-#{version}.zip",
+  url "https://github.com/rafaelSwi/MenuBarUSB/releases/download/v#{version}/MenuBarUSB.zip",
       verified: "github.com/rafaelSwi/MenuBarUSB/"
   name "MenuBarUSB"
   desc "Track your USB devices in the macOS menu bar"
