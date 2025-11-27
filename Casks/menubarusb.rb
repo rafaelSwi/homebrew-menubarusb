062a8cbfca338066de847dcdbee1ad4af0ee918b
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2025-11-27 15:08:45 -0300

    update sha for v2.15

--- Casks/menubarusb.rb
+++ Casks/menubarusb.rb
@@ -1,6 +1,6 @@
 cask "menubarusb" do
-  version "2.14"
-  sha256 "05a474ed7dabab50b388c78b9c1643470af41906431d395de28e12140b687727"
+  version "2.15"
+  sha256 "02d408da6d77126a364dff0fdeab31f2bf2d93763c614a7a804870a81df88533"
 
   url "https://github.com/rafaelSwi/MenuBarUSB/releases/download/v#{version}/MenuBarUSB.zip",
       verified: "github.com/rafaelSwi/MenuBarUSB/"
